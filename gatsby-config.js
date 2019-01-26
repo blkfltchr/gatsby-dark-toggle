71497d35921bbe60fccd5495086cb3060f6ab0b7
Blake Fletcher <blkfltchr@gmail.com>
2019-01-26 13:15:45 -0500

    Changed site metaData (title, desc, author)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Dark Toggle`,
+    description: `A playground to figure out how to use a dark/light toggle with Gatsby.`,
+    author: `@blkfltcher @skidragon @timh1203`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
